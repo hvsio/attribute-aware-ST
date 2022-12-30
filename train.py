41892ae09e263503a2af53bd60d489a8db6164cf
hvsio <alicjaharaszczuk@gmail.com>
2022-12-30 13:27:12 +0900

    wip

--- train.py
+++ train.py
@@ -152,9 +152,9 @@ def main(arg=None):
     if 'custom_set_path' in input_args:
         print('load datasets')
         train_ds = load_from_disk(
-            f"{/mnt/osmanthus/aklharas/input_args['custom_set_path']}/transformers/train_cuda:0_en_mbarttoklabel_nolower.data/train")
-        dev_ds = load_from_disk(f"{/mnt/osmanthus/aklharas/input_args['custom_set_path']}/transformers/validation_cuda:0_en_mbarttoklabel_nolower.data/train")
-        test_ds = load_from_disk(f"{/mnt/osmanthus/aklharas/input_args['custom_set_path']}/transformers/test_cuda:0_en_mbarttoklabel_nolower.data/train")
+            f"/mnt/osmanthus/aklharas/{input_args['custom_set_path']}/transformers/train_cuda:0_en_mbarttoklabel_nolower.data/train")
+        dev_ds = load_from_disk(f"/mnt/osmanthus/aklharas/{input_args['custom_set_path']}/transformers/validation_cuda:0_en_mbarttoklabel_nolower.data/train")
+        test_ds = load_from_disk(f"/mnt/osmanthus/aklharas/{input_args['custom_set_path']}/transformers/test_cuda:0_en_mbarttoklabel_nolower.data/train")
         print('datasets loaded')
         train_ds = train_ds.remove_columns(['no', 'ja_speaker', 'en_sentence', 'ja_sentence', 'ja_spkid', 'en_spkid', 'ja_wav', 'en_wav', 'ja_spk_gender', 'en_spk_gender', 'ja_spk_prefecture', 'en_spk_state'])
         dev_ds = dev_ds.remove_columns(['no', 'ja_speaker', 'en_sentence', 'ja_sentence', 'ja_spkid', 'en_spkid', 'ja_wav', 'en_wav', 'ja_spk_gender', 'en_spk_gender', 'ja_spk_prefecture', 'en_spk_state'])
@@ -165,6 +165,7 @@ def main(arg=None):
 
     training_args = TrainingArguments(
         output_dir=f"/mnt/osmanthus/aklharas/checkpoints/{input_args.get('modelpath', temp_id.strftime('%d/%m/%Y-%H.%M'))}",
+        overwrite_output_dir=True,
         per_device_train_batch_size=int(input_args['batch']),
         per_device_eval_batch_size=int(input_args['batch']),
         gradient_accumulation_steps=int(input_args['grad_accum']),
